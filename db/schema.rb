834b79cc003adfe648df63842cec860665423754
NakasaiHayato <nakasai.hayato@gmail.com>
2020-06-10 19:45:39 +0900

    カード作成機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_071643) do
+ActiveRecord::Schema.define(version: 2020_06_10_103349) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string "title", limit: 255, null: false
+    t.text "memo", limit: 1000
+    t.integer "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "title", null: false
@@ -20,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_071643) do
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
-  create_table "user", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
