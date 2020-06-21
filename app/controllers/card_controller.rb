20cde9c05021e3210b87d8d1416a085e7e87f3b2
NakasaiHayato <nakasai.hayato@gmail.com>
2020-06-21 22:16:43 +0900

    カード編集機能実装

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,4 +1,6 @@
 class CardController < ApplicationController
+  before_action :set_card, only: %i(show edit update)
+
   def new
     @card = Card.new
     @list = List.find_by(id: params[:list_id])
@@ -17,8 +19,24 @@ class CardController < ApplicationController
     @card = Card.find_by(id: params[:id])
   end
 
-  private
-  def card_params
-    params.require(:card).permit(:title, :memo, :list_id)
+  def edit
+    @card = Card.find_by(id: params[:id])
   end
+
+  def update
+    if @card.update_attributes(card_params)
+      redirect_to :root
+    else
+      render action: :edit
+    end
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:title, :memo, :list_id)
+    end
+
+    def set_card
+      @card = Card.find_by(id: params[:id])
+    end
 end
